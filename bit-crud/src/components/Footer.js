a19994db9cfed677f48e4a5cd810735b91c806d4
BIT Student <classroom@bgit.rs>
2019-12-06 13:18:42 +0100

    components set

---
+++ bit-crud/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = (props) => {
+    return (
+        <footer>
+            <h5>Footer</h5>
+        </footer>
+    )
+}
+export default Footer
---
+++ bit-crud/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = (props) => {
+    return (
+        <header>
+            <h1>Final Project</h1>
+        </header>
+    )
+}
+export default Header
