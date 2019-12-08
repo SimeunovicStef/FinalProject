58c587d9c187121e330830efe893453850c5714f
vuk.malidzan <vuk.malidzan@yahoo.com>
2019-12-08 18:19:19 +0100

    footer update

--- bit-crud/src/components/Footer.js
+++ bit-crud/src/components/Footer.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Footer = (props) => {
     return (
-        <footer>
-            <h5>Footer</h5>
-        </footer>
+        <footer class="page-footer">
+        <div class="footer-copyright">
+          <div class="container">
+          © 2019 BIT
+          <a class="grey-text text-lighten-4 right" href="#!">Sign In</a>
+          </div>
+        </div>
+      </footer>
     )
 }
 export default Footer
