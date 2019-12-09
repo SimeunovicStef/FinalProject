a20f33fb30f8cc98be11788ada5f2827d4c3f100
BIT Student <classroom@bgit.rs>
2019-12-09 12:07:21 +0100

    login create

--- bit-crud/src/app/App.js
+++ bit-crud/src/app/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Header from '../components/header/Header'
-import Main from '../components/main/Main'
 import Footer from '../components/footer/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import RegisterForm from '../page/RegisterForm';
-import LoginForm from '../page/LoginForm';
-import { Link, Switch, Route } from 'react-router-dom'
+
+import LoginPage from '../page/LoginPage'
 
 
 function App() {
@@ -13,7 +11,7 @@ function App() {
     <>
       <Header />
 
-      <RegisterForm />
+      <LoginPage />
 
       <Footer />
     </>
---
+++ bit-crud/src/page/LoginPage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Input from '../components/input/Input'
+
+
+
+
+
+class LoginPage extends React.Component {
+    render() {
+        return (
+            <div className='wraper'>
+
+                <Input />
+                <Input />
+            </div>
+        )
+    }
+}
+
+export default LoginPage
