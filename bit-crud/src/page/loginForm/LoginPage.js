c0d0f6d01db1e743fd67c00b5fd635cbdf2f9b12
Pr0xi <hawk2106@mts.rs>
2019-12-10 05:57:01 +0100

    login page update

--- bit-crud/src/page/loginForm/LoginPage.js
+++ bit-crud/src/page/loginForm/LoginPage.js
@@ -13,35 +13,86 @@ class LoginPage extends React.Component {
         super(props)
         this.state = {
             email: "",
-            password: "",
-            error: "",
+            loginPassword: "",
+            errorMsg: "",
             checkbox: false
 
         }
     }
-    onFormSubmitHandler = e => {
-        e.preventDefault();
-    }
-    onChangeInputHandler = e => {
-        this.setState({ [e.target.name]: e.target.value })
-    }
+
+    // onEmailChange = (event) =this.setState({email:event.target.value, error:''})
+    // onPassChange = (event) =this.setState({password:event.target.value, error:''})
+
+
+
+    // onFormSubmitHandler = e => {
+    //     e.preventDefault();
+    // }
+    // onChangeInputHandler = e => {
+    //     this.setState({ [e.target.name]: e.target.value })
+    // }
+
+
+  loginHandler = event => {
+    const { email, loginPassword } = this.state;
+    const content = {
+      email: email,
+      password: loginPassword
+    };
+
+    event.preventDefault();
+    this.sendLoginForm(content);
+  };
+
+  resetLoginForm = () => {
+    this.setState({
+      email: "",
+      loginPassword: "",
+      errorMsg: ""
+    });
+  };
+
+  goToHomepage = () => this.props.history.push("/");
+
+  sendLoginForm = data => {
+    const { sendLoginData } = this.props;
+
+    sendLoginData(data).then(({ error }) => {
+      if (error) {
+        return this.setState({ errorMsg: error.message });
+      }
+
+      this.resetLoginForm();
+      this.goToHomepage();
+    });
+  };
+
+  onChangeHandler = event => {
+    const elementId = event.target.id;
+    const elementValue = event.target.value;
+
+    this.setState({
+      [elementId]: elementValue
+    });
+  };
+
 
     render() {
-
+// const { errorMsg } = this.state;
         return (
             <div className='container'>
 
-                <form className='col s12' onSubmit={this.onFormSubmitHandler}>
+                <form className='col s12'>
                     <div className='wraper'>
                         <Icon className='lockLogo' />
                         <Title title="Sign In" className="title" />
                         <Row >
-                            <Input name="email" type="text" placeholder="Email Address*" value={this.state.email} onChange={this.onChangeInputHandler} />
-                            <Input name="password" type="password" placeholder="Password*" value={this.state.password} onChange={this.onChangeInputHandler} />
+                            <Input name="email" type="text" placeholder="Email Address*"  required onChange={this.onChangeHandler} />
+                            <Input name="password" type="password" placeholder="Password*" required onChange={this.onChangeHandler} />
                         </Row>
                         <Checkbox onChange={(d) => console.log(d)} label="Remember me" />
 
-                        <Button title='Sign In' />
+                        <Button title='Sign In' type="submit" onClick={this.loginHandler} name="action" />
                         <Row >
                             <a className="col 4">Forgot password?</a>
                             <span className="col 4 offset-md-4"><Link to='/sign-up'>Don't have an account? Sign Up'</Link></span>
