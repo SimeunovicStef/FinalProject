e41d547858d6d7d2fb2720ecaa670ee878645d8d
BIT Student <classroom@bgit.rs>
2019-12-10 12:30:37 +0100

    changes to routes

--- bit-crud/src/components/button/Button.js
+++ bit-crud/src/components/button/Button.js
@@ -2,7 +2,19 @@ import React from 'react'
 import './Button.css'
 
 const Button = (props) => {
-    return (<button className={props.className} onClick={props.onClick}> {props.title}</button>)
+    return (<button className={
+        props.className
+    }
+
+        onClick={
+            props.onClick
+        }
+
+    > {
+            props.title
+        }
+
+    </button>)
 }
 
 export default Button
