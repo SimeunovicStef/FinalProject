d9c34636edbfd3d037fc6b221e289b28f2ef073c
BIT Student <classroom@bgit.rs>
2019-12-12 11:03:10 +0100

    adding dash header

--- bit-crud/src/page/Dashboard/DashboardLanding.js
+++ bit-crud/src/page/Dashboard/DashboardLanding.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Title from "../../components/title/Title"
-
+import Header from '../../components/header/Header'
 import Dashboard from "./Dashboard";
+import { Link, withRouter } from 'react-router-dom'
+
 
 const DashboardLanding = () => (
   <div className="container">
+
+
     <Title title="Dashboard" />
     <Dashboard />
 
